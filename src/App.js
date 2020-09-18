e1f13c1a077f24aa9d01381be48ea7edd72bd968
Dan Kiss <flowfast47@gmail.com>
2020-09-18 15:47:49 -0700

    add components and testing with Enzyme and React-Testing-Library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/button/button'
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>This is a counter app</h1>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button id="decrement-btn" onClick={() => setCounter(counter - 1)}>Decrement</button>
+      
+      <Button label="Click me" />
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,37 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//configure enzyme
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+describe('Counter testing', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  test('renders learn react link', () => {
+    expect(wrapper.find('h1').text()).toContain('This is a counter app')
+  });
+
+  test('render a button with a text of `increment`', () => {
+    expect(wrapper.find('#increment-btn').text()).toBe('Increment')
+  })
+
+  test('render the intial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe("0")
+  })
+
+  test('render the click event of increment button and increment counter value', () => {
+    wrapper.find('#increment-btn').simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('1')
+  })
+
+  test('render the click event of decrement button and decrement counter value', () => {
+    wrapper.find('#decrement-btn').simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('-1')
+  })
+
 });
